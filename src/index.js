86f4c26d9b2a093c4126f36fc8ee7de7fe4f13a0
aya <aya.gamal1794@gmail.com>
2023-05-03 03:43:24 +0300

    edit

--- src/index.js
+++ src/index.js
@@ -4,16 +4,14 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/bootstrap/dist/js/bootstrap.min";
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-    <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<App/>}/>
-        </Routes>
-    </BrowserRouter>
+    <HashRouter base="/">
+        <App/>
+    </HashRouter>
     
 );
 
